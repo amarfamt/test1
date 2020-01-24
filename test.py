f4241470c23e67dc47bfff79b82c73568a31d54a
amarfamt <amarpalwankar1296@gmail.com>
2020-01-24 17:25:24 +0530

    okoko

--- test.py
+++ test.py
@@ -2,5 +2,6 @@ print("Success")
 print("Achieved")
 print("OKOKoKO")
 print("Domo for Jenkins today on Friday")
+print("Just a demo for Jenkins")
 for i in range(0,5):
 	print("Success today on Thurday modified")

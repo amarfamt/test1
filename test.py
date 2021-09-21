ce9555de2e97818610507e146a2cd0ade6a21aa0
amarfamt <53889701+amarfamt@users.noreply.github.com>
2021-09-21 16:41:40 +0530

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-print("Success")
-print("Achieved")
-print("OKOKoKO")
-print("Domo for Jenkins today on Friday")
-print("Just a demo for Jenkins")
-for i in range(0,5):
-	print("Success today on Thurday modified")

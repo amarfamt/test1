53c20f34c19e1ba2ed550ce18af38afebc0562fa
amarfamt <53889701+amarfamt@users.noreply.github.com>
2022-09-20 12:49:02 +0530

    sonar

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-def startF():
-  pass
-def start1():
-  ++i
 
+from flask import request
+import os
+
+@app.route('/ping')
+def ping():
+    address = request.args.get("address")
+    cmd = "ping -c 1 %s" % address
+    os.popen(cmd) # Noncompliant
